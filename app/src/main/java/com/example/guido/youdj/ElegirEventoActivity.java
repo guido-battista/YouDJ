8e397de4b75519f522634e2be84e8a9536f48e40
guido-battista <guido.battista@hotmail.com>
2018-07-29 11:12:27 -0300

    Agrego icono y splash

--- app/src/main/java/com/example/guido/youdj/ElegirEventoActivity.java
+++ app/src/main/java/com/example/guido/youdj/ElegirEventoActivity.java
@@ -33,6 +33,7 @@ public class ElegirEventoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_elegir_evento);
         nroEvento = (TextView) findViewById(R.id.nroEvento);
         codigoEvento = (TextView) findViewById(R.id.codigoEvento);
